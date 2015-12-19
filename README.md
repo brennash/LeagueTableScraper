a14de4bc4fb5f8f70618a378b6b97452f5712903
Ubuntu <shane.brennan.irl@gmail.com>
2015-12-19 00:36:57 +0000

    First attempt to get the league table SQL working

--- README.md
+++ README.md
@@ -6,9 +6,9 @@ series of tables representing the various fixtures and tables scraped.
 
 To install and run this code, ideally on Ubuntu 14.04, you need to install or have, 
 
-sudo apt-get install python-pip
-sudo apt-get install git
-sudo apt-get install python-mysqldb
+- sudo apt-get install python-pip
+- sudo apt-get install git 
+- sudo pip install BeautifulSoup
 
 The example database is found in the conf directory and can be setup with the commands in the sql/ folder. 
 
@@ -16,12 +16,19 @@ The example database is found in the conf directory and can be setup with the co
 
 Install a MySQL database on the local test environment if there's not already one there
 
-sudo apt-get install mysql-server
-sudo apt-get install mysql-client
+- sudo apt-get install mysql-server
+- sudo apt-get install mysql-client
+- sudo apt-get install python-mysqldb
 
 Setup a database, noting the name, and add a new user/password, all this setup SQL is detailed in 
 the sql folder. 
 
 ## Running the Scraper
 
+python IRLFixtureScraper
+
+or, if you want to speficy the database
+
 python IRLFixtureScraper conf/database.conf
+
+I'm writing this code half-cut after a few bottles of wine, so quality may suffer a bit. What the hell though.
--- sql/createTables.sql
+++ sql/createTables.sql
@@ -5,3 +5,93 @@ GRANT ALL PRIVILEGES ON * . * TO 'username'@'localhost';
 
 --- Now create the fixture tables
 create table 2015_IRL0_Fixtures (FixtureSID INT AUTO_INCREMENT, LeagueCode VARCHAR(6) NOT NULL, SeasonCode INT NOT NULL, Date DATE, HomeTeam VARCHAR(64) NOT NULL,  AwayTeam VARCHAR(64) NOT NULL, HomeFT INT NOT NULL, AwayFT INT NOT NULL, HomeHT INT, AwayHT INT, Venue VARCHAR(64), PRIMARY KEY(FixtureSID, LeagueCode , SeasonCode));
+
+--- Now the league table
+create table 2015_IRL0_Table (
+  TableSID INT NOT NULL AUTO_INCREMENT, 
+  LeagueCode VARCHAR(6), 
+  SeasonCode INT, 
+  TeamName VARCHAR(64), 
+  Win INT, 
+  Draw INT, 
+  Loss INT, 
+  GoalsFor INT, 
+  GoalsAgainst INT, 
+  GoalDiff INT, 
+  Points INT, 
+  HomeWin INT, 
+  HomeDraw INT, 
+  HomeLoss INT, 
+  HomeGoalsFor INT, 
+  HomeGoalsAgainst INT, 
+  HomeGoalDiff INT,
+  HomePoints INT, 
+  AwayWin INT, 
+  AwayDraw INT, 
+  AwayLoss INT, 
+  AwayGoalsFor INT, 
+  AwayGoalsAgainst INT,
+  AwayGoalDiff INT, 
+  AwayPoints INT, 
+PRIMARY KEY(TableSID, LeagueCode, SeasonCode));
+
+--- Get all the distinct teams into a temporary table
+CREATE TEMPORARY TABLE IF NOT EXISTS TempTeams AS (SELECT DISTINCT(HomeTeam) FROM 2015_IRL0_Fixtures);
+
+
+--- Create the procedure to populate the league table
+DROP PROCEDURE IF EXISTS UpdateLeagueTable;
+DELIMITER ;;
+CREATE PROCEDURE UpdateLeagueTable()
+BEGIN
+DECLARE n INT DEFAULT 0;
+DECLARE i INT DEFAULT 0;
+
+DECLARE hwin INT DEFAULT 0;
+DECLARE hdraw INT DEFAULT 0;
+DECLARE hloss INT DEFAULT 0;
+DECLARE hgf INT DEFAULT 0;
+DECLARE hga INT DEFAULT 0;
+
+DECLARE awin INT DEFAULT 0;
+DECLARE adraw INT DEFAULT 0;
+DECLARE aloss INT DEFAULT 0;
+DECLARE agf INT DEFAULT 0;
+DECLARE aga INT DEFAULT 0;
+
+DECLARE name VARCHAR(64) DEFAULT NULL;
+SELECT COUNT(*) FROM TempTeams INTO n;
+SET i=0;
+WHILE i < n DO
+	SELECT HomeTeam FROM TempTeams LIMIT i,1 INTO name;
+
+	-- Get the home details
+	SELECT COUNT(*) FROM 2015_IRL0_Fixtures WHERE homeTeam=name and homeFT>awayFT INTO hwin;
+	SELECT COUNT(*) FROM 2015_IRL0_Fixtures WHERE homeTeam=name and homeFT=awayFT INTO hdraw;
+	SELECT COUNT(*) FROM 2015_IRL0_Fixtures WHERE homeTeam=name and homeFT<awayFT INTO hloss;
+	SELECT SUM(homeFT) FROM 2015_IRL0_Fixtures WHERE homeTeam=name INTO hgf;
+	SELECT SUM(awayFT) FROM 2015_IRL0_Fixtures WHERE homeTeam=name INTO hga;
+
+	-- Now get the away details
+	SELECT COUNT(*) FROM 2015_IRL0_Fixtures WHERE awayTeam=name and homeFT<awayFT INTO awin;
+	SELECT COUNT(*) FROM 2015_IRL0_Fixtures WHERE awayTeam=name and homeFT=awayFT INTO adraw;
+	SELECT COUNT(*) FROM 2015_IRL0_Fixtures WHERE awayTeam=name and homeFT>awayFT INTO aloss;
+	SELECT SUM(awayFT) FROM 2015_IRL0_Fixtures WHERE awayTeam=name INTO agf;
+	SELECT SUM(homeFT) FROM 2015_IRL0_Fixtures WHERE awayTeam=name INTO aga;
+
+
+	INSERT INTO 2015_IRL0_Table 
+	  (LeagueCode, SeasonCode, TeamName, 
+		--- Win,     Draw,     Loss,     GoalsFor,     GoalsAgainst,     GoalDiff,     Points, 
+		HomeWin, HomeDraw, HomeLoss, HomeGoalsFor, HomeGoalsAgainst, HomeGoalDiff, HomePoints,
+		AwayWin, AwayDraw, AwayLoss, AwayGoalsFor, AwayGoalsAgainst, AwayGoalDiff, AwayPoints) 
+	VALUES 
+	  ('IRL0', 2015, name, 
+		--- (hwin+awin), (hdraw+adraw), (hloss+aloss), (hgf+agf), (hga+aga), (hgf-hga+agf-aga), ((hwin+awin)*3)+(hdraw+adraw),
+		hwin, hdraw, hloss, hgf, hga, (hgf-hga), (hwin*3+hdraw),
+		awin, adraw, aloss, agf, aga, (agf-aga), (awin*3+adraw));
+	SET i = i + 1;
+END WHILE;
+End;
+;;
+DELIMITER ;
