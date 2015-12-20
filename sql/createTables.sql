6e895e5a56b32678ccd1cf0de9bd3bfd3487847d
Ubuntu <shane.brennan.irl@gmail.com>
2015-12-20 16:18:07 +0000

    Updating SQL for Team table

--- sql/createTables.sql
+++ sql/createTables.sql
@@ -4,10 +4,25 @@ create database mydatabase
 GRANT ALL PRIVILEGES ON * . * TO 'username'@'localhost';
 
 --- Now create the fixture tables
-create table 2015_IRL0_Fixtures (FixtureSID INT AUTO_INCREMENT, LeagueCode VARCHAR(6) NOT NULL, SeasonCode INT NOT NULL, Date DATE, HomeTeam VARCHAR(64) NOT NULL,  AwayTeam VARCHAR(64) NOT NULL, HomeFT INT NOT NULL, AwayFT INT NOT NULL, HomeHT INT, AwayHT INT, Venue VARCHAR(64), PRIMARY KEY(FixtureSID, LeagueCode , SeasonCode));
+create table 2015_IRL0_Fixtures 
+(
+  FixtureSID INT AUTO_INCREMENT, 
+  LeagueCode VARCHAR(6) NOT NULL, 
+  SeasonCode INT NOT NULL, 
+  Date DATE, 
+  HomeTeam VARCHAR(64) NOT NULL,
+  AwayTeam VARCHAR(64) NOT NULL, 
+  HomeFT INT NOT NULL, 
+  AwayFT INT NOT NULL, 
+  HomeHT INT, 
+  AwayHT INT, 
+  Venue VARCHAR(64), 
+  PRIMARY KEY(FixtureSID, LeagueCode , SeasonCode)
+);
 
 --- Now the league table
-create table 2015_IRL0_Table (
+create table 2015_IRL0_Table 
+(
   TableSID INT NOT NULL AUTO_INCREMENT, 
   LeagueCode VARCHAR(6), 
   SeasonCode INT,
@@ -42,39 +57,39 @@ create table 2015_IRL0_Table (
   MovedDown TINYINT(1),
   StayedSame TINYINT(1),
   Comment VARCHAR(128),
-PRIMARY KEY(TableSID, LeagueCode, SeasonCode));
-
+PRIMARY KEY(TableSID, LeagueCode, SeasonCode)
+);
 
 --- Populate the league table
 DROP PROCEDURE IF EXISTS UpdateLeagueTable;
 DELIMITER ;;
 CREATE PROCEDURE UpdateLeagueTable()
-BEGIN
-DECLARE n INT DEFAULT 0;
-DECLARE i INT DEFAULT 0;
+  BEGIN
+  DECLARE n INT DEFAULT 0;
+  DECLARE i INT DEFAULT 0;
 
-DECLARE h_win INT DEFAULT 0;
-DECLARE h_draw INT DEFAULT 0;
-DECLARE h_loss INT DEFAULT 0;
-DECLARE h_gf INT DEFAULT 0;
-DECLARE h_ga INT DEFAULT 0;
+  DECLARE h_win INT DEFAULT 0;
+  DECLARE h_draw INT DEFAULT 0;
+  DECLARE h_loss INT DEFAULT 0;
+  DECLARE h_gf INT DEFAULT 0;
+  DECLARE h_ga INT DEFAULT 0;
 
-DECLARE a_win INT DEFAULT 0;
-DECLARE a_draw INT DEFAULT 0;
-DECLARE a_loss INT DEFAULT 0;
-DECLARE a_gf INT DEFAULT 0;
-DECLARE a_ga INT DEFAULT 0;
+  DECLARE a_win INT DEFAULT 0;
+  DECLARE a_draw INT DEFAULT 0;
+  DECLARE a_loss INT DEFAULT 0;
+  DECLARE a_gf INT DEFAULT 0;
+  DECLARE a_ga INT DEFAULT 0;
 
-DECLARE name VARCHAR(64) DEFAULT NULL;
+  DECLARE name VARCHAR(64) DEFAULT NULL;
 
-CREATE TEMPORARY TABLE IF NOT EXISTS TempTeams AS (SELECT DISTINCT(HomeTeam) FROM 2015_IRL0_Fixtures);
+  CREATE TEMPORARY TABLE IF NOT EXISTS TempTeams AS 
+  (SELECT DISTINCT(HomeTeam) FROM 2015_IRL0_Fixtures);
 
-SELECT COUNT(*) FROM TempTeams INTO n;
+  SELECT COUNT(*) FROM TempTeams INTO n;
 
-SET i=0;
-WHILE i < n DO
+  SET i=0;
+  WHILE i < n DO
 	SELECT HomeTeam FROM TempTeams LIMIT i,1 INTO name;
-
 	SELECT COUNT(*) FROM 2015_IRL0_Fixtures WHERE homeTeam=name and homeFT>awayFT INTO h_win;
 	SELECT COUNT(*) FROM 2015_IRL0_Fixtures WHERE homeTeam=name and homeFT=awayFT INTO h_draw;
 	SELECT COUNT(*) FROM 2015_IRL0_Fixtures WHERE homeTeam=name and homeFT<awayFT INTO h_loss;
@@ -86,7 +101,6 @@ WHILE i < n DO
 	SELECT COUNT(*) FROM 2015_IRL0_Fixtures WHERE awayTeam=name and homeFT>awayFT INTO a_loss;
 	SELECT SUM(awayFT) FROM 2015_IRL0_Fixtures WHERE awayTeam=name INTO a_gf;
 	SELECT SUM(homeFT) FROM 2015_IRL0_Fixtures WHERE awayTeam=name INTO a_ga;
-
 
 	INSERT INTO 2015_IRL0_Table 
 	  (LeagueCode, SeasonCode, TeamName, 
@@ -119,4 +133,16 @@ DELIMITER ;
 -- Now call the function to update the table
 CALL UpdateLeagueTable();
 
-create table 2015_IRL0_Teams (TeamSID INT NOT NULL AUTO_INCREMENT, TeamName VARCHAR(64), TeamLogoURL VARCHAR(256), HomeJerseyURL VARCHAR(256), AwayJerseyURL VARCHAR(256), Stadium VARCHAR(64), Manager VARCHAR(64), Honours VARCHAR(512), PRIMARY KEY(TeamSID));
+create table 2015_IRL0_Teams 
+(
+  TeamSID INT NOT NULL AUTO_INCREMENT, 
+  TeamName VARCHAR(64), 
+  TeamLogoURL VARCHAR(256), 
+  HomeJerseyURL VARCHAR(256), 
+  AwayJerseyURL VARCHAR(256), 
+  Stadium VARCHAR(64), 
+  Manager VARCHAR(64), 
+  Honours VARCHAR(512), 
+  PRIMARY KEY(TeamSID)
+);
+
